6c00e721cc30e402efb245d46695954a0d3b4f28
me-sayanghosh <sayanghosh1887@gmail.com>
2026-07-26 17:23:15 +0530

    FIX 1

--- src/ui/lamp.jsx
+++ src/ui/lamp.jsx
@@ -14,7 +14,7 @@ export const LampContainer = ({ children, className }) => {
       <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
         <motion.div
           initial={{ opacity: 0.5, width: "15rem" }}
-          whileInView={{ opacity: 1, width: "30rem" }}
+          whileInView={{ opacity: 0.5, width: "30rem" }}
           transition={{
             delay: 0.3,
             duration: 0.8,
